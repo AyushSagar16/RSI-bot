ffbe6f303c9783f0b064c6bef8eec3b4e6ca5bc0
Ayush Sagar <ayushsagar3018@gmail.com>
2024-07-20 19:22:04 +0530

    Created classes to access content faster and better and easier

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import yfinance as yf
 ts = TimeSeries(key='')
 # Get json object with the intraday data and another with  the call's metadata
 data, meta_data = ts.get_intraday('GOOGL')
+#RSI = 100 – [100 ÷ ( 1 + (Average Gain During Up Periods ÷ Average Loss During Down Periods ))]
 
 periods = ["minutes", "hours"]
 
@@ -13,5 +14,13 @@ msft = yf.Ticker("MSFT")
 # get all stock info
 information = msft.info
 hist = msft.history(period="1mo")
-print(hist)
+print(type(hist))
+
+class RSI():
+    # This class will be used to get the RSI of different stocks and it will probably be only one method big
+    def __init__(self, stockName):
+        self.sName = stockName
+
+    def getPrice(self):
+        return price
 
